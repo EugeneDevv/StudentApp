f80219110d894ac8be0ca01830d1c333eb5b9415
EugeneDevv <eugenemrlens@gmail.com>
2020-07-17 05:32:38 -0700

    Initial Cimmit

---
+++ app/src/main/java/com/eujoh/studentapp/HelperClasses/HomeAdapter/UpcomingEventsHelperClass.java
@@ -0,0 +1,38 @@
+package com.eujoh.studentapp.HelperClasses.HomeAdapter;
+
+import android.widget.TextView;
+
+public class UpcomingEventsHelperClass {
+    int image;
+    String title, description;
+
+    public UpcomingEventsHelperClass(int image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/eujoh/studentapp/User/announcements.java
@@ -0,0 +1,16 @@
+package com.eujoh.studentapp.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.eujoh.studentapp.R;
+
+public class announcements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.announcements);
+    }
+}
