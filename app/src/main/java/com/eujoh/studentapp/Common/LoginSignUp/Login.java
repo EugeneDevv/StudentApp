fdc4c6d37e60350d53fb78fb656859f7d02ff5a7
EugeneDevv <eugenemrlens@gmail.com>
2020-07-26 03:03:01 +0300

    after forget password screen

--- app/src/main/java/com/eujoh/studentapp/Common/LoginSignUp/Login.java
+++ app/src/main/java/com/eujoh/studentapp/Common/LoginSignUp/Login.java
@@ -2,8 +2,12 @@ package com.eujoh.studentapp.Common.LoginSignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import com.eujoh.studentapp.User.UserDashboard;
 
 import com.eujoh.studentapp.R;
 
@@ -13,6 +17,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.student_login);
+        setContentView(R.layout.login);
+    }
+
+    public void callForgetPassword(View view) {
+        startActivity(new Intent(this, ForgetPassword.class));
+    }
+
+    public void callUserDashBoard(View view) {
+        startActivity(new Intent(this, UserDashboard.class));
+    }
+
+    public void callSignUp(View view) {
+        startActivity(new Intent(this,SignUp.class));
     }
 }
